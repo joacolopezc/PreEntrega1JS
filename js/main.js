19f04eb3d3ec5ca069c63d419fe2906e39d3d26d
Joaco Lopez <joaquincamilotto@gmail.com>
2024-07-12 02:29:09 +0200

    v1.0

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const solicitante = prompt("Ingrese su nombre")
+
+function saludarSolicitante() {
+    alert("¡Bienvenido/a " + solicitante + ", sigue los pasos para encontrar mas informacion!")
+}
+
+saludarSolicitante()
+
